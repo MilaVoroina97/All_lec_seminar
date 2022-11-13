f37cfddd0b71b67f8b54ade083ce2a757cb10f17
mila <milavoronina1997@mail.ru>
2022-11-13 18:55:12 +0500

    seminar 6

--- seminar6.py
+++ seminar6.py
@@ -58,37 +58,37 @@
 #     (1+2)*3 => 9;
 
 
-task = '1+2*3'
-task.replace(' ','')
+# task = '1+2*3'
+# task.replace(' ','')
 
-if '*' in task:
-    a = task.index('*')
-    answer = int(task[a-1]) * int(task[a+1])
-    task = task[0:a-1] + str(answer) + task[a+2:]
-    print(task)
+# if '*' in task:
+#     a = task.index('*')
+#     answer = int(task[a-1]) * int(task[a+1])
+#     task = task[0:a-1] + str(answer) + task[a+2:]
+#     print(task)
 
-if '/'in task:
-    a = task.index('/')
-    answer = int(task[a-1]) / int(task[a+1])
-    task = task[0:a-1] + str(answer) + task[a+2:]
-    print(task)
+# if '/'in task:
+#     a = task.index('/')
+#     answer = int(task[a-1]) / int(task[a+1])
+#     task = task[0:a-1] + str(answer) + task[a+2:]
+#     print(task)
 
-if '+' in task:
-    parts = task.split('+')
-    for i in parts:
-        if '-' in i:
-            def_parts = i.split('-')
-            dif = 0
-            for j in parts:
-                dif -= int(j)
-            i = dif
-    print(dif)
-    sum_parts = 0
-    for i in parts:
-        sum_parts += int(i)
-    print(sum_parts)
+# if '+' in task:
+#     parts = task.split('+')
+#     for i in parts:
+#         if '-' in i:
+#             def_parts = i.split('-')
+#             dif = 0
+#             for j in parts:
+#                 dif -= int(j)
+#             i = dif
+#     print(dif)
+#     sum_parts = 0
+#     for i in parts:
+#         sum_parts += int(i)
+#     print(sum_parts)
 
     # a = task.index('+')
     # answer = int(task[:a]) + int(task[a+1:])#все что до конца и все что до начала 
 
-    
+print([i for i in [i for i in zip([1,2,3], [4,5,6], [2,3,5]) if i for j in i] if i for i in i])
